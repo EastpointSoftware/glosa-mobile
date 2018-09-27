0d64c5a3a05298125f61118bc4a4d765b0006694
Jin khera <Jin.khera@gmail.com>
2018-09-27 14:29:59 +0100

    add prebuld script

---
+++ GreenLight/GreenLight.Droid/appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+﻿if [ ! -n "$API_URL" ]
+then
+    echo "You need define the API_URL variable in App Center"
+    exit
+fi
+
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/GreenLight.Core/Constants.cs
+
+if [ -e "$APP_CONSTANT_FILE" ]
+then
+    echo "Updating ApiUrl to $API_URL in AppConstant.cs"
+    sed -i '' 's#ApiUrl = "[a-z:./]*"#ApiUrl = "'$API_URL'"#' $APP_CONSTANT_FILE
+
+    echo "File content:"
+    cat $APP_CONSTANT_FILE
+fi
