702d8f6141e1d38226ff55ff71caccaf261f5584
Jin khera <Jin.khera@gmail.com>
2018-09-27 14:30:05 +0100

    add prebuld script

--- GreenLight/GreenLight.Droid/appcenter-pre-build.sh
+++ GreenLight/GreenLight.Droid/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
-﻿if [ ! -n "$API_URL" ]
+﻿if [ ! -n "$AZURE_MOBILE_SERVICE_CLIENT_URI" ]
 then
     echo "You need define the API_URL variable in App Center"
     exit
@@ -8,8 +8,13 @@ APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/GreenLight.Core/Constants.cs
 
 if [ -e "$APP_CONSTANT_FILE" ]
 then
-    echo "Updating ApiUrl to $API_URL in AppConstant.cs"
-    sed -i '' 's#ApiUrl = "[a-z:./]*"#ApiUrl = "'$API_URL'"#' $APP_CONSTANT_FILE
+    echo "Updating values $AZURE_MOBILE_SERVICE_CLIENT_URI in Constants.cs"
+    sed -i '' 's#AZURE_MOBILE_SERVICE_CLIENT_URI = "[a-z:./]*"#AZURE_MOBILE_SERVICE_CLIENT_URI = "'$AZURE_MOBILE_SERVICE_CLIENT_URI'"#' $APP_CONSTANT_FILE
+	sed -i '' 's#API_GLOSA_MAP_ENDPPOINT_URL = "[a-z:./]*"#API_GLOSA_MAP_ENDPPOINT_URL = "'$API_GLOSA_MAP_ENDPPOINT_URL'"#' $APP_CONSTANT_FILE
+	sed -i '' 's#API_GLOSA_SPAT_ENDPPOINT_URL = "[a-z:./]*"#API_GLOSA_SPAT_ENDPPOINT_URL = "'$API_GLOSA_SPAT_ENDPPOINT_URL'"#' $APP_CONSTANT_FILE
+	sed -i '' 's#API_GLOSA_CAM_ENDPPOINT_URL = "[a-z:./]*"#API_GLOSA_CAM_ENDPPOINT_URL = "'$API_GLOSA_CAM_ENDPPOINT_URL'"#' $APP_CONSTANT_FILE
+	sed -i '' 's#ApiUrl = "[a-z:./]*"#AZURE_APP_CENTER_IOS_KEY = "'$AZURE_APP_CENTER_IOS_KEY'"#' $APP_CONSTANT_FILE
+	sed -i '' 's#ApiUrl = "[a-z:./]*"#AZURE_APP_CENTER_ANDROID_KEY = "'$AZURE_APP_CENTER_ANDROID_KEY'"#' $APP_CONSTANT_FILE
 
     echo "File content:"
     cat $APP_CONSTANT_FILE
